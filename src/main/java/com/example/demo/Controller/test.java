cc5ebae70686743ffa94d8b615a236e5e86a55c2
SMNrootQYS <song_mn@yeah.net>
2021-04-02 14:29:11 +0800

    提交最初版本

---
+++ src/main/java/com/example/demo/Controller/test.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("test")
+public class test {
+    @ResponseBody
+    @RequestMapping("/smn")
+    public String test1(){
+        return "testsmn";
+    }
+}
